91f107ff05c4b7e9efb68574233c49a2c0ae30f0
hridoycse197 <hridoycse.eng@gmail.com>
2022-09-28 18:08:17 +0600

    willpop

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
@@ -31,6 +33,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int i = 0;
+  DateTime timeBackPressed = DateTime.now();
+  late WebViewController controllerGlobal;
+  final Completer<WebViewController> _controllerCompleter =
+      Completer<WebViewController>();
   @override
   void initState() {
     if (Platform.isAndroid) {
@@ -41,11 +48,109 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: WebView(
-          javascriptMode: JavascriptMode.unrestricted,
-          initialUrl: 'https://khazakhaled.com/',
+    return WillPopScope(
+      onWillPop: () async {
+        final difference = DateTime.now().difference(timeBackPressed);
+        timeBackPressed = DateTime.now();
+        final isExitWarning = difference >= Duration(seconds: 2);
+
+       
+          if (await controllerGlobal.canGoBack()) {
+            controllerGlobal.goBack();
+            i = 0;
+            return false;
+          } else {
+            if (i == 0) {
+              setState(() {
+                i = 1;
+              });
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => MyHomePage1(),
+                ),
+              );
+              return false;
+            } else {
+              return true;
+            }
+          }
+        
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: WebView(
+            onWebViewCreated: (WebViewController webViewController) {
+              controllerGlobal = webViewController;
+            },
+            javascriptMode: JavascriptMode.unrestricted,
+            initialUrl: 'https://khazakhaled.com/',
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage1 extends StatefulWidget {
+  const MyHomePage1({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage1> createState() => _MyHomePage1State();
+}
+
+class _MyHomePage1State extends State<MyHomePage1> {
+  late WebViewController controllerGlobal;
+  final Completer<WebViewController> _controllerCompleter =
+      Completer<WebViewController>();
+  DateTime timeBackPressed = DateTime.now();
+  int i = 0;
+  @override
+  void initState() {
+    if (Platform.isAndroid) {
+      WebView.platform = SurfaceAndroidWebView();
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        final difference = DateTime.now().difference(timeBackPressed);
+        timeBackPressed = DateTime.now();
+        final isExitWarning = difference >= Duration(seconds: 2);
+
+   
+          if (await controllerGlobal.canGoBack()) {
+            controllerGlobal.goBack();
+            i = 0;
+            return false;
+          } else {
+            if (i == 0) {
+              setState(() {
+                i = 1;
+              });
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => MyHomePage1(),
+                ),
+              );
+              return false;
+            } else {
+              return true;
+            }
+          }
+        
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: WebView(
+            onWebViewCreated: (WebViewController webViewController) {
+              controllerGlobal = webViewController;
+            },
+            javascriptMode: JavascriptMode.unrestricted,
+            initialUrl: 'https://khazakhaled.com/',
+          ),
         ),
       ),
     );
