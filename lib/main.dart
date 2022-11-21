b32e3bdab34a536dc0145ce39650bacd0933b37f
mhrhabib39 <mhrhabib39@gmail.com>
2022-11-21 10:44:07 +0600

    hello

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: WebView(
           javascriptMode: JavascriptMode.unrestricted,
           initialUrl: 'https://khazakhaled.com/',
+          //hello
         ),
       ),
     );
